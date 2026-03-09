9b8b758e3eb7032c13b6ebe0b7b4487e68fa4f44
Bhavya Anil <bhavya.anil2023@gmail.com>
2026-03-08 19:26:28 -0700

    fixed player movement

--- CMPM147-Roguelike/Assets/Scripts/PlayerMovement.cs
+++ CMPM147-Roguelike/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     DungeonGenerator generator;
     Vector2Int currentGridPos;
     float spacing = 1.5f;
-    bool inputLocked = false;
+    bool isMoving = false;
+
     void Start()
     {
         generator = FindObjectOfType<DungeonGenerator>();
@@ -18,51 +19,53 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isMoving) return; 
         Vector2Int dir = Vector2Int.zero;
 
-        if (!inputLocked)
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            dir = Vector2Int.up;
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            dir = Vector2Int.down;
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            dir = Vector2Int.left;
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            dir = Vector2Int.right;
+
+        if (dir != Vector2Int.zero)
         {
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-                dir = Vector2Int.up;
-
-            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-                dir = Vector2Int.down;
-
-            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-                dir = Vector2Int.left;
-
-            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-                dir = Vector2Int.right;
-
-            if (dir != Vector2Int.zero)
+            Vector2Int nextPos = currentGridPos + dir;
+            if (IsConnected(currentGridPos, nextPos))
             {
-                TryMove(dir);
-                inputLocked = true;
+                StartCoroutine(MoveTo(nextPos));
             }
-        }
-
-        // unlock input when keys are released
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow) ||
-            Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow) ||
-            Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow) ||
-            Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            inputLocked = false;
         }
     }
-    void TryMove(Vector2Int dir)
-    {
-        Vector2Int nextPos = currentGridPos + dir;
 
+    bool IsConnected(Vector2Int from, Vector2Int to)
+    {
         foreach (var edge in generator.corridors)
         {
-            if ((edge.Item1 == currentGridPos && edge.Item2 == nextPos) ||
-                (edge.Item2 == currentGridPos && edge.Item1 == nextPos))
-            {
-                currentGridPos = nextPos;
-                transform.position = new Vector3(nextPos.x * spacing, nextPos.y * spacing, 0);
-                return;
-            }
+            if ((edge.Item1 == from && edge.Item2 == to) || (edge.Item2 == from && edge.Item1 == to))
+                return true;
         }
+        return false;
+    }
+    System.Collections.IEnumerator MoveTo(Vector2Int nextPos)
+    {
+        isMoving = true;
+        Vector3 start = transform.position;
+        Vector3 end = new Vector3(nextPos.x * spacing, nextPos.y * spacing, 0);
+        float t = 0f;
+        float speed = 2f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * speed;
+            transform.position = Vector3.Lerp(start, end, Mathf.Min(t, 1f));
+            yield return null;
+        }
+
+        currentGridPos = nextPos;
+        isMoving = false;
     }
 }
